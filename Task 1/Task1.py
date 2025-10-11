1a44f548043ed344d7cb29d020bb385a9df8311a
Kushagra Srivastava <kushagra.3nov2004@gmail.com>
2025-10-11 12:35:18 +0530

    Add files via upload

---
+++ Task 1/Task1.py
@@ -0,0 +1,11 @@
+
+#Asking for user Input 
+name=input("Enter your Name: ")
+age=input("Enter your Age: ")
+hobby=input("Enter your Hobby: ")
+
+#displaying introduction message
+print("\nHello", name + "!")
+print(f"You're {age} years old and you love {hobby}.")
+print("Welcome to the community!")
+
